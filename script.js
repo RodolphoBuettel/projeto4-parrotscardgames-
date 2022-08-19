d5e723aaa442b6bf3d7c11f9fab9e422af823204
Rodolpho Piermattei Buettel <rodolpho05buettel@gmail.com>
2022-08-19 13:53:17 -0300

    Cartas Embaralhadas

--- script.js
+++ script.js
@@ -6,20 +6,30 @@ while(numeroDecartas < 4 || numeroDecartas > 14 || numeroDecartas % 2 !== 0){
 
     numeroDecartas = Number(numeroDecartas);
 }
-
-//const distribuiCartas = [];
-//let contador = 0;
-
-    //while( numeroDecartas > contador){
-    //distribuiCartas.push(contador);
-    //console.log(distribuiCartas);
-    //contador++;
-    //}
+    let indice = 0;
+    let contador = 0;
     
-    let = contador = 0;
+    let array = [];
+    console.log(array);
+    
+    let imagens = ['<img src = "./imagens/bobrossparrot.gif"/>','<img src = "./imagens/bobrossparrot.gif"/>','<img src = "./imagens/explodyparrot.gif"/>','<img src = "./imagens/explodyparrot.gif"/>','<img src = "./imagens/fiestaparrot.gif"/>','<img src = "./imagens/fiestaparrot.gif"/>','<img src = "./imagens/metalparrot.gif"/>','<img src = "./imagens/metalparrot.gif"/>','<img src = "./imagens/revertitparrot.gif"/>','<img src = "./imagens/revertitparrot.gif"/>','<img src = "./imagens/tripletsparrot.gif"/>','<img src = "./imagens/tripletsparrot.gif"/>','<img src = "./imagens/unicornparrot.gif"/>','<img src = "./imagens/unicornparrot.gif"/>'];
+    while(numeroDecartas > contador){
+        array.push(`${imagens[indice]}`);
+        contador ++;
+        indice ++;
+    }
+    array.sort(comparador);
 
+    function comparador(){
+        return Math.random() - 0.5;
+    }
+    contador = 0;
+    indice = 0;
     const baralho = document.querySelector('.baralho');
     while(numeroDecartas > contador){
-        baralho.innerHTML = baralho.innerHTML + `<div class = "carta${contador}" onclick="virarcarta(this)"><img src="./imagens/front.png"/></div>`; 
+        baralho.innerHTML = baralho.innerHTML + `<div class = "carta${contador}">
+        <img src="./imagens/front.png"/><div class = "imagem-fundo">${array[indice]}</div></div>`;
+        
+        indice++; 
         contador++;
     }
